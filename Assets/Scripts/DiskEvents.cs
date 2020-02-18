63fc94a5911d2ff8dafce46d5f8cddd450b9aded
Tom Chitty <tom.chitty@epitech.eu>
2020-02-18 13:37:48 +0100

    disk cannot go out + limit speed

---
+++ Assets/Scripts/DiskEvents.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiskEvents : MonoBehaviour
+{
+    public float maxSpeed = 15f;
+    private Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void onGrabbed()
+    {
+        transform.eulerAngles = Vector3.zero;
+    }
+
+    public void onReleased()
+    {
+        transform.eulerAngles = Vector3.zero;
+        
+        if (rigidbody.velocity.magnitude > maxSpeed) {
+            rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
+        }
+    }
+
+    public void onGrabMove()
+    {
+        transform.eulerAngles = Vector3.zero;
+    }
+
+}
