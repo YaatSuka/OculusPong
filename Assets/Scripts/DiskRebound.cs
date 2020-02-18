5379f1f7ce651a8dc44038ad441ecec1d18ccbed
Tom Chitty <tom.chitty@epitech.eu>
2020-02-18 10:56:11 +0100

    testing for disk rebound

---
+++ Assets/Scripts/DiskRebound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiskRebound : MonoBehaviour
+{
+    public Vector3 myVelocity = 5.0f;
+ 
+    private void Update ()
+    {
+        // Move
+        transform.position += myVelocity * Time.deltaTime;
+    
+        // Look in the direction it's moving in
+        transform.rotation = Quaternion.LookRotation (myVelocity);
+    }
+    
+    private void OnTriggerEnter (Collider other)
+    {
+        myVelocity.z *= -1;  // Flip
+    }
+}
