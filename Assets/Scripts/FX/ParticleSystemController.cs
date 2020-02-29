65ed16de39da5440d9a482ac60454ca83f0bd2bf
Tom Chitty <tom.chitty@epitech.eu>
2020-02-29 17:12:07 +0100

    add fx: disk with trail + impact on walls

---
+++ Assets/Scripts/FX/ParticleSystemController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemController : MonoBehaviour
+{
+    public float lifeTime = 1.0f;
+    ParticleSystem particleSystem;
+    // Start is called before the first frame update
+    void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+        particleSystem.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lifeTime -= Time.deltaTime;
+        if (lifeTime <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/FX/WallImpact.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallImpact : MonoBehaviour
+{
+    public GameObject impactParticlePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+        Vector3 contactPoint = contact.point;
+
+        Instantiate(impactParticlePrefab, contactPoint, transform.rotation);
+    }
+}
